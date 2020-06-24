3add6d4527b0be1cef3d5617a2c288c3ce8661f6
Gustavo Oliveira <gustavogiovanne3@gmail.com>
2020-06-24 13:20:55 -0300

    feat: added theme for theme provider from styled-components

---
+++ src/themes/index.ts
@@ -0,0 +1,7 @@
+/** Theme for Styled Component colors acessible via props */
+export const theme = {
+  darkDeep: '#303841',
+  darkShallow: '#3a4750',
+  emphasys: '#f6c90e',
+  light: '#eeeeee',
+};
