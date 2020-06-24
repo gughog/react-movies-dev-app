e4b4b54d8bb8dbae5f21d98bd65c73a1c2173f26
Gustavo Oliveira <gustavogiovanne3@gmail.com>
2020-06-24 15:39:21 -0300

    feat: added "darkDeepest" in theme

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,6 +1,7 @@
 /** Theme for Styled Component colors acessible via props */
 export const theme = {
   darkDeep: '#303841',
+  darkDeepest: '#272e36',
   darkShallow: '#3a4750',
   emphasys: '#f6c90e',
   light: '#eeeeee',
