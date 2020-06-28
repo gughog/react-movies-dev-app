1a6c533027a2f9e96da96658442bdad1967b4b11
Gustavo Oliveira <gustavogiovanne3@gmail.com>
2020-06-28 10:19:29 -0300

    feat: added react router and setup initial routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
 import * as React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'themes';
-
 import { Navbar, Footer } from 'components';
-import { HomePage } from 'screens';
+import { HomePage, MovieList } from 'screens';
 import { GlobalStyle } from 'components/global-styles';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 
 const App = () => (
   <ThemeProvider theme={theme}>
     <GlobalStyle fontColor={theme.light} bodyColor={theme.darkDeepest} />
-    <Navbar title="MOVIES DEV" />
-    <HomePage />
-    <Footer />
+    <Router>
+      <Navbar title="MOVIES DEV" />
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/search">
+          <MovieList pageTitle="Searching" />
+        </Route>
+        <Route path="/favorites">
+          <MovieList pageTitle="My Favorites" />
+        </Route>
+        <Route path="/watchlist">
+          <MovieList pageTitle="My Watch List" />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
   </ThemeProvider>
 );
 
