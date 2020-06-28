08035eaa1a4bc89e0ffdee2da8693f29c9a19e21
Gustavo Oliveira <gustavogiovanne3@gmail.com>
2020-06-28 11:54:49 -0300

    feat: added search page and search input

---
+++ src/components/SearchInput/styles.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const SearchContainer = styled.form`
+  display: flex;
+  justify-content: center;
+
+  @media only screen and (max-width: 800px) {
+    flex-direction: column;
+  }
+`;
+
+export const SearchInputText = styled.input`
+  display: block;
+  height: 3rem;
+  width: 50vw;
+  font-size: 1.5rem;
+  box-sizing: border-box;
+
+  color: ${({ theme }) => theme.darkShallow};
+
+  padding: 0.2rem 0.5rem;
+  margin-right: 1rem;
+  border: none;
+  border-radius: 0.5rem;
+
+  @media only screen and (max-width: 800px) {
+    width: 100%;
+    margin-right: 0;
+  }
+`;
--- src/screens/SearchPage/styles.ts
+++ src/screens/SearchPage/styles.ts
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 
-export const MovieListTitle = styled.h2`
+export const SearchPageTitle = styled.h2`
   font-size: 2rem;
   margin: 1.5rem 0;
+  text-align: center;
 `;
 
-export const MovieListResultsText = styled.p`
+export const SearchPageResultsText = styled.p`
   font-size: 1.2rem;
   margin: 1.5rem 0;
 `;
